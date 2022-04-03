aa460c055303e5a59ec91d848f2c6d53068edd54
Adroaldo de Andrade <adroaldof@gmail.com>
2022-04-03 20:33:25 -0300

    Add test faker helper file

--- test_batches.py
+++ test_batches.py
@@ -1,21 +1,21 @@
 from datetime import date
 from model import Batch, OrderLine
-from faker import Faker
 
-fake = Faker()
+from test_faker import fake, mock_sentence, mock_uuid
 
 
 def make_batch_and_line(sku: str, batch_quantity: int, line_quantity: int):
+    batch_name = mock_sentence()
+
     return (
-        Batch(fake.name(), sku, quantity=batch_quantity, purchase_date=date.today()),
+        Batch(batch_name, sku, quantity=batch_quantity, purchase_date=date.today()),
         OrderLine(fake.unique.first_name(), sku, line_quantity),
     )
 
 
 def test_allocating_to_a_batch_reduces_the_available_quantity():
-    sku = fake.name()
 
-    batch, line = make_batch_and_line(sku, 20, 2)
+    batch, line = make_batch_and_line(mock_uuid(), 20, 2)
 
     batch.allocate(line)
 
